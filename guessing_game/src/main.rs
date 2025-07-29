81c0e9e9efb48fa7a5de0a9aaed9c1b60d59b1ba
Inwerejosic <inwerejosic@protonmail.com>
2025-07-29 22:04:29 +0100

    I have tried my hands on the rand crate

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,13 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 
 fn main() {
+
+    let secret_number = rand::thread_rng().gen_range(1, 1001);
+
+
     // Declare a mutable String to store input
     let mut azrs = String::new();
 
@@ -20,5 +27,13 @@ fn main() {
         println!("I love Learning");
     } else {
         println!("Out of here");
+    }
+    // println!("The secretnumer is: {}", secret_number);
+
+    
+    match azrs.cmp(&secret_number) {
+        Ordering::Less => println!("Too Small oh"),
+        Ordering::Greater => println!("Too big oh"),
+        Ordering::Equal => println!("You are spot on!")
     }
 }
