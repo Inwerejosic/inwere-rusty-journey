e72dffddd63ce4e0159153eca79354481414039d
Inwerejosic <inwerejosic@protonmail.com>
2025-07-30 14:15:55 +0100

    Now it is all fine and efficient with the inclusion of the loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,36 +9,41 @@ fn main() {
     
     // Instantiating the random number generator.
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    // Prompting the use for input
-    println!("Kinly input your guess!");
+
+    loop {
+        println!("Kinly input your guess!");
 
     // Declare a mutable String to store input
-    let mut azrs = String::new();
+        let mut azrs = String::new();
 
     // Read input from stdin
-    io::stdin()
-        .read_line(&mut azrs)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut azrs)
+            .expect("Failed to read line");
 
     // Trim the input to remove newline and parse to i32
-    let azrs: i32 = azrs
-        .trim()
-        .parse()
-        .expect("Please enter a valid number");
+        let azrs: i32 = azrs
+            .trim()
+            .parse()
+            .expect("Please enter a valid number");
 
     // Compare the parsed number
-    if azrs >= 2 {
-        println!("I love Learning");
-    } else {
-        println!("Out of here");
-    }
+        if azrs >= 2 {
+            println!("I love Learning");
+        } else {
+            println!("Out of here");
+        }
     // println!("The secretnumer is: {}", secret_number);
 
     
-    match azrs.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small oh"),
-        Ordering::Greater => println!("Too big oh"),
-        Ordering::Equal => println!("You are spot on!")
+        match azrs.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small oh"),
+            Ordering::Greater => println!("Too big oh"),
+            Ordering::Equal => {
+                println!("You are spot on!");
+                break;
+            }
+        }
     }
+    
 }
