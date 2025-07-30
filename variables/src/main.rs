3fd3672623c0a443c2a8f4618a51ebe63d380158
Inwerejosic <inwerejosic@protonmail.com>
2025-07-30 15:09:14 +0100

    Calling a function inside a function

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,4 +9,11 @@ fn main() {
     }
 
     println!("The value of X is:, {x}");
+
+    // Calling a function inside a function
+    print_labled_measurement(6, 'm');
+
+    fn print_labled_measurement(value:i32, unit_label:char){
+        println!("The measurement is: {value}{unit_label}");
+    }
 }
