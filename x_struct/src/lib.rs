a66c8452c0596c7ad45c4e966ca0aa7ba2f2bd08
Inwerejosic <inwerejosic@protonmail.com>
2025-08-18 11:10:26 +0100

    Now making great progress

---
+++ x_struct/src/lib.rs
@@ -0,0 +1,14 @@
+#![allow(unused)]
+
+#[derive(Debug)]
+pub struct Account {
+    pub address: String,
+    pub balance: u32,
+}
+
+pub fn new(address:String) -> Account {
+    Account {
+        address,
+        balance: 0,
+    }
+}
